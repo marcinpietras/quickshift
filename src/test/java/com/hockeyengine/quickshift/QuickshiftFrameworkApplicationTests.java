81a7a654bb2c1509f9421bd563ccbd8c75bf4a2d
Marcin Pietras <mpietras@lendingclub.com>
2020-09-08 13:34:06 -0700

    Initial commit

---
+++ src/test/java/com/hockeyengine/quickshift/QuickshiftFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hockeyengine.quickshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickshiftFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
